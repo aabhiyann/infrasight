71daad9ae452c36458ebe0f90278a1fe8f448827
aabhiyann <sainjuabhiyan321@gmail.com>
2025-06-20 09:38:13 -0400

    setting up postgress

---
+++ backend/db.py
@@ -0,0 +1,22 @@
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+# Loading secrets from the .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create Async Engine
+engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+
+# Session Factory
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+# Calling this on startup to create all tables
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
