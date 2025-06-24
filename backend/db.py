401483600a9878c907d71b0759c1f3a25cf1b06e
aabhiyann <sainjuabhiyan321@gmail.com>
2025-06-24 14:02:17 -0400

    db.py removing creatig tables and init db; just session creation now

--- backend/db.py
+++ backend/db.py
@@ -1,25 +1,22 @@
-from sqlmodel import SQLModel
-from sqlmodel.ext.asyncio.session import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.ext.declarative import DeclarativeMeta
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
-from sqlalchemy import text
 from dotenv import load_dotenv
 import os
 
-# Loading secrets from the .env file
+
+# Loading environment variables
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Create Async Engine
-engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+# Creating Async Engine
+engine = create_async_engine(DATABASE_URL, echo=True, poolclass=NullPool)
 
-# Session Factory
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+# Creating Async Session Factory
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-# Calling this on startup to create all tables
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+# Dependency for FastAPI routes
+async def get_session():
+    async with AsyncSessionLocal() as session:
+        yield session
