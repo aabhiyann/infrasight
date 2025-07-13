00f33dd46794b5aa55fdb51f46cef8db3ab5d052
aabhiyann <sainjuabhiyan321@gmail.com>
2025-07-13 10:36:26 -0400

    adding clusters route to main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import log, insights, mock_data
+from routes import log, insights, mock_data, clusters
 
 
 app = FastAPI()
@@ -7,5 +7,7 @@ app = FastAPI()
 app.include_router(log.router, prefix="/api")
 app.include_router(insights.router, prefix="/api")
 app.include_router(mock_data.router, prefix="/api")
+app.include_router(clusters.router, prefix="/api")
+
 
 
